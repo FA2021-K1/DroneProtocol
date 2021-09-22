4c190fd05098ffe3debab1357dd72c0077536550
Ole <ole.petersen@go4more.de>
2021-09-22 17:48:31 +0200

    Rename to DroneProtocol

---
+++ Sources/DroneProtocol/DroneProtocol.swift
@@ -0,0 +1,9 @@
+import CoatySwift
+
+ /// Save a reference of your container in the app delegate to
+ /// make sure it stays alive during the entire lifetime of the app.
+var coatyContainer: Container?
+
+class iDroneControl {
+    let protocolVersion = 1
+}
---
+++ Tests/DroneProtocolTests/DroneProtocolTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import DroneProtocol
+
+    final class DroneProtocolTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+            //XCTAssertEqual(iDroneControl().text, "Hello, World!")
+        }
+    }
